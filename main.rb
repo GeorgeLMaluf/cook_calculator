411eb20f223f5f8eb7d9c2fde64329b7029fbdb9
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2022-11-16 14:57:13 -0300

    Adicionado arquivos principais ao projeto

---
+++ main.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+puts 'Miojo CookCalculator'
+puts 'George L. Maluf - v 0.1.1'
+3.times { puts '' }
+puts 'Informe o tempo de cozimento'
+puts 'Informe o tempo da primeira ampulheta'
+puts 'Informe o tempo da segunda ampulheta'
