8f22774532f9a1df4020b98d976eb6df64c48b83
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2022-11-16 19:30:13 -0300

    Adicionado metodo de calculo e respectivos testes

--- main.rb
+++ main.rb
@@ -10,11 +10,11 @@ puts 'Informe o tempo da primeira ampulheta'
 hour_glass_a = gets.to_i
 puts 'Informe o tempo da segunda ampulheta'
 hour_glass_b = gets.to_i
-cook_timer = CookTimer.new(time_to_cook: time,
-                           hour_glass_one: hour_glass_a,
-                           hour_glass_two: hour_glass_b)
+cook_timer = CookTimer.new(time, hour_glass_a, hour_glass_b)
 if !cook_timer.solvable?
   puts 'Não há como resolver com os parametros informados'
 else
-  puts ''
+  minimum_cook_time = cook_timer.cook_time
+  puts "O tempo mínimo de preparo é #{minimum_cook_time}"
 end
+puts ''
--- spec/lib/cook_timer_spec.rb
+++ spec/lib/cook_timer_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe CookTimer do
   describe '#solvable?' do
     context 'when has solution' do
       let(:subject) do
-        CookTimer.new(time_to_cook: 3,
-                      hour_glass_one: 5,
-                      hour_glass_two: 7)
+        CookTimer.new(3, 5, 7)
       end
 
       it 'expect true' do
@@ -18,13 +16,41 @@ RSpec.describe CookTimer do
 
     context 'when has no solution' do
       let(:subject) do
-        CookTimer.new(time_to_cook: 3,
-                      hour_glass_one: 5,
-                      hour_glass_two: 5)
+        CookTimer.new(3, 5, 5)
       end
 
       it 'expect false' do
         expect(subject.solvable?).to be_falsey
+      end
+    end
+  end
+
+  describe '#cook_time' do
+    context 'when cooking time takes 1 minute' do
+      let(:time) { 1 }
+      it 'with hourglass of 3 and 5 minutes, it will take at least 6 minutes' do
+        expect(CookTimer.new(time, 3, 5).cook_time).to eq 6
+      end
+    end
+
+    context 'when cooking time takes 3 minutes' do
+      let(:time) { 3 }
+      it 'with hourglass of 5 and 7 minutes, it will take at least 10 minutes' do
+        expect(CookTimer.new(time, 5, 7).cook_time).to eq 10
+      end
+    end
+
+    context 'when cooking time takes 5 minutes' do
+      let(:time) { 5 }
+      it 'with hourglass of 7 and 9 minutes, it will take at least 14 minutes' do
+        expect(CookTimer.new(time, 7, 9).cook_time).to eq 14
+      end
+    end
+
+    context 'when cooking time takes 15 minutes' do
+      let(:time) { 15 }
+      it 'with hourglass of 25 and 30 minutes, it will take at least 75 minutes' do
+        expect(CookTimer.new(time, 25, 30).cook_time).to eq 75
       end
     end
   end
